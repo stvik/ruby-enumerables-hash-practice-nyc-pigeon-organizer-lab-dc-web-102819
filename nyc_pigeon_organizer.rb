8cd161077ac4eb042e1b0317cf3ae3f6f29a22a7
Sara Tarnvik <stvik2@gmail.com>
2019-10-18 14:22:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  new_hash = {}
+  pigeons = data[:gender][:male] + data[:gender][:female]
+  pigeons.each do |pigeon|
+    new_hash[pigeon] = {color: [], gender: [], lives: []}
+    if data[:gender][:male].include?(pigeon)
+      new_hash[pigeon][:gender] = ["male"]
+    else
+      new_hash[pigeon][:gender] = ["female"]
+    end
+    data[:color].each do |color|
+      if color.include?(pigeon)
+        new_hash[pigeon][:color] = ["white"]
+      end
+    end
+  end
+
+    
+  
+  new_hash
 end
