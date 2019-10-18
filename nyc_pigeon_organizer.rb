1906019fb1a59f73160e6075106659cf9ce7a8dc
Sara Tarnvik <stvik2@gmail.com>
2019-10-18 14:36:46 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,34 +4,23 @@ def nyc_pigeon_organizer(data)
   pigeons = data[:gender][:male] + data[:gender][:female]
   pigeons.each do |pigeon|
     new_hash[pigeon] = {color: [], gender: [], lives: []}
-    #if data[:gender][:male].include?(pigeon)
-     # new_hash[pigeon][:gender] = ["male"]
-   # else
-    #  new_hash[pigeon][:gender] = ["female"]
- #   end
- #   data[:color].each do |key, value|
-  #    if value.include?(pigeon)
-  #      new_hash[pigeon][:color].push(key.to_s)
-  #    end
-  #  end
- #   data[:lives].each do |key, value|
- #     if value.include?(pigeon)
- #       new_hash[pigeon][:lives].push(key.to_s)
- #     end
- #   end
-    
-    data.each do |hash|
-      hash.each do |key, value|
-        if value.include?(pigeon)
-        new_hash[pigeon][hash].push(key.to_s)
+    if data[:gender][:male].include?(pigeon)
+      new_hash[pigeon][:gender] = ["male"]
+    else
+      new_hash[pigeon][:gender] = ["female"]
+    end
+    data[:color].each do |key, value|
+      if value.include?(pigeon)
+        new_hash[pigeon][:color].push(key.to_s)
       end
-      end
-   end 
+    end
+    data[:lives].each do |key, value|
+      if value.include?(pigeon)
+       new_hash[pigeon][:lives].push(key.to_s)
+     end
+   end
     
-    
-  end
-
-    
+ 
   
   new_hash
 end
